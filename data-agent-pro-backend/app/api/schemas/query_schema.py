4b64f62d08fdde965502aa1210e32e8efc867adf
winkey <winkeytan123@gmail.com>
2026-06-29 12:23:44 +0800

    Five major module improvements

--- data-agent-pro-backend/app/api/schemas/query_schema.py
+++
@@ -1,5 +0,0 @@
-from pydantic import BaseModel
-
-
-class QuerySchema(BaseModel):
-    query: str
--- data-agent-pro-backend/app/models/qdrant/metric_info_qdrant.py
+++
@@ -1,9 +0,0 @@
-from typing import TypedDict
-
-
-class MetricInfoQdrant(TypedDict):
-    id: str
-    name: str
-    description: str
-    relevant_columns: list
-    alias: list
